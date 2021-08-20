cc6936c948c27135175cf26d812fe1b24333d8b6
RAVISH KUMAR <89187239+kumar777ravish@users.noreply.github.com>
2021-08-20 20:37:54 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,23 +4,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OrganisationConsoleUI1
+namespace Data_Type_And_Control_Structures
 {
+    struct Student
+    {
+        public int StudentId;
+        public string StudentName;
+        public double Avg;
+
+        public void Details()
+        {
+            Console.WriteLine("Student Id " + StudentId)
+            Console.WriteLine("Student Name " + StudentName);
+            Console.WriteLine("Avg" + Avg);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Employee E = new Employee()
-            {
-                EmpId = 100,
-                Name = "Peter",
-                SalaryPerDay = 32.5,
-                HRA = 12.3
-            };
 
-            double GS = E.GetSalary(3);
-            Console.WriteLine("Gross salary of {0} is {1}", E.Name, GS);
+            var vs classes:-
+
+            //var x = "Ravish";
+            //var z = 10;
+            //z = 45
+            //var x = new int[6, 7];
+            //dynamic x;
+            //x = 67;
+            //x = "Ravish";
+
+            Add(45, 67);
+
+            Add(45, 67.8);
+
+            Add(67.8, 56);
+
+            Add(67.9, 23.9);
+
+            Add("Ravish " "kumar");
+
             Console.ReadLine();
-        }
-    }
+
+
+           
 }
+
+
+
+
+
+
+
+
+
+
+
+{ }
+   
+       
