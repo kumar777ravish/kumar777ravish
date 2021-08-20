438248f3c98310f78c9f214ae4b58f8b6bce1a6a
RAVISH KUMAR <89187239+kumar777ravish@users.noreply.github.com>
2021-08-20 20:23:56 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganisationConsoleUI1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee E = new Employee()
+            {
+                EmpId = 100,
+                Name = "Peter",
+                SalaryPerDay = 32.5,
+                HRA = 12.3
+            };
+
+            double GS = E.GetSalary(3);
+            Console.WriteLine("Gross salary of {0} is {1}", E.Name, GS);
+            Console.ReadLine();
+        }
+    }
+}
