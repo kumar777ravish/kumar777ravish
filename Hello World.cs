c3f519bfd39160a5fa45125df7754621fe6a9a9d
RAVISH KUMAR <89187239+kumar777ravish@users.noreply.github.com>
2021-08-20 20:48:15 +0530

    Add files via upload

---
+++ Hello World.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWorld");
+            Console.ReadLine();
+            Console.WriteLine("HelloWorld");
+            Console.ReadLine();
+            Console.WriteLine("HelloWorld");
+            Console.ReadLine();
+            Console.WriteLine("HelloWorld");
+            Console.WriteLine();
+
+
+            
+
+        }
+    }
+}
