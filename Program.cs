245f8df0494e11511ac219941b7f56b6f2459749
RAVISH KUMAR <89187239+kumar777ravish@users.noreply.github.com>
2021-08-20 20:50:41 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,271 +4,127 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp6
+namespace OOP_s1
 {
+    class Student
+    {
+        int Sid;
+        string Name;
+        double M1, M2;
+
+        public void CreateStudent(int id, string name)
+
+        public void SetMarks(double m1, double m2)
+
+        public void Results()
+    }
     class Program
     {
-        static void Main(string[] args)
+            Student S;
+            S = new Student();
+
+            Console.WriteLine("Enter Sid And Name");
+            int id = int.Parse(Console.ReadLine());
+            string name = Console.ReadLine();
+
+            S.CreateStudent(id, name);
+
+            Console.WriteLine("Enter Marks 1 and 2");
+            double m1 = double.Parse(Console.ReadLine());
+            double m2 = double.Parse(Console.ReadLine());
+
+            S.SetMarks(m1, m2);
+
+            S.Result();
+            S.CreateStudent(200, "Ravish"); //Re-initialise the object - issue 2
+
+
+            //Student S1 = new Student();
+            //S1.CreateStudent(200, "Ravish");
+            Console.WriteLine("Hello World!");
+        }
+
+        class,Fields,Methods And Objects
+
+        class Student
         {
-            //int a, b, c;
-            //a = 10;
-            //b = 20;
-            //c = a + b;
-
-            //Console.WriteLine("The Sum Of {0} and {1} is {2}", a, b, c);
-
-            //Console.WriteLine("The Sum Of " + a + " and " + b + " is " + c);
-
-            int a, b, c;
-            a = int.Parse(Console.ReadLine());
-            b = int.Parse("45");
-            c = a + b;
-
-            Console.WriteLine("The Sum Of {0} and {1} is {2}", a, b, c);
-
-            Console.WriteLine("The Sum Of " + a + " and " + b + " is " + c);
-
-            Console.ReadLine();
+            int Sid;
+            string Name;
+            double M1, M2;
+        }
+        public Student(int id, string name)
+        {
+            Sid = id;
+            name = name;
+        }
+        public void SetMarks(double m1, double m2)
+        {
+            M1 = m1;
+            M2 = m2;
+        }
+        public void Result()
+        {
+            duble avg = (M1 + M2) / 2;
+            if (avg >= 35)
             {
-                Day1 - Introduction
-
-                //Dim i as integer;
-                //int i;
-
-                //System.Int32 i;
-                //Dim i as System.Int32;
-
-                // int a = 10
-                //int b = 20
-                //if (a>b)
-                //{
-                //    Console.WriteLine("{0} is Greater",a);
-                //}
-                //else
-                //{
-                //   Console.WriteLine("{0} is Greater",b);
-                //}
-
-                int a = 10;
-                int b = 20;
-                int c = 6;
-                int d = 56;
-                //if (a > b && a > c && a > d)
-                //{
-                //    Console.WriteLine("{0} is Geater", a);
-                //}
-                //else
-                //{
-                //    if (b > a && b > c && b > d)
-                //    {
-                //        Console.WriteLine("{0} is Greater", b);
-                //    }
-                //    else
-                //    {
-                //        if (c > a && c > b && c > d)
-                //        {
-                //            Console.WriteLine("{0} is Greater", c);
-                //        }
-                //        else
-                //        {
-                //            Console.WriteLine("{0} is Greater", d);
-                //        }
-                //    }
-                //}
-
-                //if (a > b && a > c && a > d)
-                //{
-                //    Console.WriteLine("{0} is Geater", a);
-                //}
-                //else if (b > a && b > c && b > d)
-                //{
-                //    Console.WriteLine("{0} is Greater", b);
-                //}
-                //else if (c > a && c > b && c > d)
-                //{
-                //    Console.WriteLine("{0} is Greater", c);
-                //}
-                //else
-                //{
-                //    Console.WriteLine("{0} is Greater", d);
-                //}
-
-                //int a = 10;
-                //int b = 20;
-
-                //int opt;
-
-                //L: Console.WriteLine("1.Add 2.Mul 3.Sub 4.Exit);
-                //Opt = int.Parse(Console.ReadLine());
-
-                //switch (opt)
-                //{
-                //    case 2:
-                //        Console.WriteLine("Multiplication is " * a * b);
-                //        break;
-                //    case 1:
-                //        Console.WriteLine("Addition is " + (a + b));
-                //        break;
-                //    case 3:
-                //        Console.WriteLine("sub is " + (a - b));
-                //        break;
-                //    case 4:
-                //        break;
-                //    default:
-                //        Console.Writeline("Invalid Option ");
-                //        goto L;
-                //}
-
-                //for (int i = 0; i < 10; i * *)
-                //{
-                //    Console.Writeline("MTT");
-                //}
-
-                ////6* 1 = 6
-                ////6* 2 = 12
-                //int n = 6;
-                //for (int i = 1; i <= 10; i++)
-                //{
-                //    Console.WriteLine(" {0} * {1} = {2}", n, i, n * i);
-                //}
-                //Console.WriteLine("-------------------------------------");
-                //int n = 6;
-                //for (int i = 10; i >= 1; i--)
-                //{
-                //    Console.WriteLine(" {0} * {1} = {2}", n, i, n * i);
-                //}
-
-                int n = 2367; ;
-
-                Console.WriteLine(2367/10);
-                Console.Writeline(2367 % 10);
-                //sum=10
-                //r=n%10=7
-                //sum =sum +r=7
-                //n=n/10=236
-
-                //r=n%10=7
-                //sum =sum +r=13
-                //n=n/10=23
-
-                //r=n%10=7
-                //sum =sum +r=16
-                //n=n/10=2
-
-                //r=n%10=7
-                //sum =sum +r=18
-                //n=n/10=0
-
-                //int sum = 0;
-                //int r = 0;
-                //while (n != 0
-                //{
-                //    r = n % 10;
-                //    sum = sum + r;
-                //    n = n / 10;
-                //}
-                //Console.WriteLine("Sum is " + sum);
-
-                //int i;
-                //do
-                //{
-                //    i * int.Parse(Console.ReadLine())
-                //} while (i!=0);
-
-                //for (; ; )
-                //{
-                //    Console.Write("MTT   ");
-                //}
-
-                while (true)
-                {
-                    Console.Write("MTT   ");
-                }
-                //Console.ReadLine();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                    Console.ReadLine();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                {
-
-
-++++
-                        }
+                Console.WriteLine("Pass");
+            }
+            else
+            {
+                Console.WriteLine("Fail");
+            }
+        }
+    }
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student S = new Student(100, "peter");
+        S.SetMarks(23, 45);
+
+        S.Result();
+        //S.Student(200, "Ravish");
+
+        Console.ReadLine();
     }
 }
-        +++++++++++++++++
 
 
 
+
+
+
+
+          
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+    
+    
+        
+        
+    
+
+        
+       
+    
+
+
+    
+    {
+       
+        {
+        }
+    }
+}
